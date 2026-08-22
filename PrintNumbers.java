365e01f259590364e3bb4b1f573f6e9b7e027b08
Sonali Chakraborty <sonalchakraborty2826@gmail.com>
2026-08-22 19:05:45 +0530

    assignment3

---
+++ PrintNumbers.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class PrintNumbers {
+    static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++)
+            System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter N: ");
+        printNumbersUpToN(sc.nextInt());
+        sc.close();
+    }
+}
