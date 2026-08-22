65350a5c3782bcc504a0f5cdbc31cb10805044a8
Sonali Chakraborty <sonalchakraborty2826@gmail.com>
2026-08-22 19:05:29 +0530

    assignment2

---
+++ NumberClassifier.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class NumberClassifier {
+    static void classifyNumber(int number) {
+        if (number > 0)
+            System.out.println("Positive");
+        else if (number < 0)
+            System.out.println("Negative");
+        else
+            System.out.println("Zero");
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        classifyNumber(sc.nextInt());
+        sc.close();
+    }
+}
