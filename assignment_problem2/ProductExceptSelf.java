975419612ab99aaf6722825c9ac599a9efac2d5e
Sonali Chakraborty <sonalchakraborty2826@gmail.com>
2026-08-29 16:05:37 +0530

    1st problem

---
+++ assignment_problem2/ProductExceptSelf.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class ProductExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+
+        int n = nums.length;
+        int[] answer = new int[n];
+
+        // Forward pass: product of elements on the left
+        int leftProduct = 1;
+
+        for (int i = 0; i < n; i++) {
+            answer[i] = leftProduct;
+            leftProduct = leftProduct * nums[i];
+        }
+
+        // Backward pass: product of elements on the right
+        int rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            answer[i] = answer[i] * rightProduct;
+            rightProduct = rightProduct * nums[i];
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 2, 3, 4};
+
+        int[] result = productExceptSelf(nums);
+
+        System.out.println(Arrays.toString(result));
+    }
+}
